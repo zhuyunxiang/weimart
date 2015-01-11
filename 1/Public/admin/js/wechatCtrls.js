372d284fdc533997e7380ab829f3038ced1445a5
493405455 <zhuyunxiang@users.noreply.github.com>
2015-01-11 14:58:18 +0000

    1. angularjs 动态修改

--- 1/Public/admin/js/wechatCtrls.js
+++ 1/Public/admin/js/wechatCtrls.js
@@ -2,9 +2,34 @@ var WechatCtrls = angular.module('WechatCtrls', []);
 
 WechatCtrls.controller('wechatCtrl', ['$scope',
 	function($scope){
-		$scope.menuList = [{name: "信息1",secondMenuList:[{name: "sm"},{name: "sm"},{name: "sm"}]},{name: "信息2",secondMenuList:[{name: "sm"},{name: "sm"},{name: "sm"}]}];
+		$scope.menuList = [
+		{
+			id: 111,
+			name: "信息1",
+			secondMenuList:
+				[
+					{id: 112,name: "sm1"},
+					{id: 113,name: "sm2"},
+					{id: 114,name: "sm3"}
+				]
+		},
+		{
+			name: "信息2",
+			secondMenuList:
+				[
+					{id: 115,name: "sm4"},
+					{id: 116,name: "sm5"},
+					{id: 117,name: "sm6"}
+				]
+		}];
 		$scope.edit_first_munu = function (index) {
-			$scope.editMenuName = $scope.menuList[index].name;
+			$scope.editMenuName = $scope.menuList[index].name;	
+			$scope.editMenuId = $scope.menuList[index].id;
+
+		}
+		$scope.edit_second_munu = function (parentIndex,index) {
+			$scope.editMenuName = $scope.menuList[parentIndex].secondMenuList[index].name;
+			$scope.editMenuId = $scope.menuList[parentIndex].secondMenuList[index].id;
 		}
 	}
 ]);
