66089434bea3f924c80f9e21046f384c26034391
493405455@qq.com <493405455@qq.com@b65dde92-2312-485a-a083-f5afe3003cf0>
2015-01-29 16:07:35 +0000

    1. 添加文字菜单修改

--- 1/Public/admin/js/wechatCtrls.js
+++ 1/Public/admin/js/wechatCtrls.js
@@ -1,233 +1,268 @@
 var WechatCtrls = angular.module('WechatCtrls', []);
 
-WechatCtrls.controller('wechatCtrl', ['$scope','$timeout','$http',
-	function($scope,$timeout,$http){
-		getInfo();
+WechatCtrls.controller('wechatCtrl', ['$scope', '$timeout', '$http',
+    function($scope, $timeout, $http) {
+        getInfo();
 
-		function getInfo () {
-			$http({
-		        method  : 'POST',
-		        url     : getMenuListUrl,
-		        data    : null,
-		        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-		    })
-	        .success(function(data) {
-	 			$scope.menuList = data.data.menuList;
-	        });
-		}
+        function getInfo() {
+            $http({
+                method: 'POST',
+                url: getMenuListUrl,
+                data: null,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                } // set the headers so angular passing info as form data (not request payload)
+            })
+                .success(function(data) {
+                    $scope.menuList = data.data.menuList;
+                });
+        }
 
-		$scope.tips = {
-			errorShow : false,
-			successShow : false,
-			leftTime: 4
-		}
+        $scope.tips = {
+            errorShow: false,
+            successShow: false,
+            leftTime: 4
+        }
 
-		function showTipsTimeout () {
-			var t = $timeout(showTipsTimeout,1000);
-			if ($scope.tips.leftTime > 0) {
-				$scope.tips.leftTime--;
-			} else {
-				$timeout.cancel(t);
-				$scope.closeAllTips();
-			}
-		}
+        function showTipsTimeout() {
+            var t = $timeout(showTipsTimeout, 1000);
+            if ($scope.tips.leftTime > 0) {
+                $scope.tips.leftTime--;
+            } else {
+                $timeout.cancel(t);
+                $scope.closeAllTips();
+            }
+        }
 
-		function showTips (type,text) {
-			if (type == 'success') {
-				$scope.closeAllTips();
-				$scope.tips.successShow = true;
-				$scope.tips.Text = text;
-				showTipsTimeout();
-			} else {
-				$scope.closeAllTips();
-				$scope.tips.errorShow = true;
-				$scope.tips.Text = text;
-				showTipsTimeout();
-			}
-		}
+        function showTips(type, text) {
+            if (type == 'success') {
+                $scope.closeAllTips();
+                $scope.tips.successShow = true;
+                $scope.tips.Text = text;
+                showTipsTimeout();
+            } else {
+                $scope.closeAllTips();
+                $scope.tips.errorShow = true;
+                $scope.tips.Text = text;
+                showTipsTimeout();
+            }
+        }
 
-		$scope.defaultCss = {
-			hasChild: false,
-			toChoose: false
-		}
+        $scope.defaultCss = {
+            hasChild: false,
+            toChoose: false
+        }
 
-		$scope.closeAllTips = function () {
-			$scope.tips = {
-				errorShow : false,
-				successShow : false,
-				leftTime: 4
-			}
-		}
+        $scope.closeAllTips = function() {
+            $scope.tips = {
+                errorShow: false,
+                successShow: false,
+                leftTime: 4
+            }
+        }
 
-		$scope.add_first_menu = function () {
-			if ($scope.menuList == null || $scope.menuList.length < 3) {
-				$scope.menuModalTitle = "添加一级菜单";
-				$scope.editMenuInfo = {
-					editMenuType : 'addFirst',
-					editMenuName: null,
-					editMenuId : null,
-					editMenuParentId : null,
-				}
-				$("#menuModal").modal('show');
-			} else {
-				showTips('error',"对不起，最多只能有3个一级菜单！");
-			}
-		}
+        $scope.add_first_menu = function() {
+            if ($scope.menuList == null || $scope.menuList.length < 3) {
+                $scope.menuModalTitle = "添加一级菜单";
+                $scope.editMenuInfo = {
+                    editMenuType: 'addFirst',
+                    editMenuName: null,
+                    editMenuId: null,
+                    editMenuParentId: null,
+                }
+                $("#menuModal").modal('show');
+            } else {
+                showTips('error', "对不起，最多只能有3个一级菜单！");
+            }
+        }
 
-		$scope.edit_first_menu = function (index) {
-			$scope.menuModalTitle = "修改一级菜单";
-			$scope.editMenuInfo = {
-				editMenuType : 'editFirst',
-				editMenuName: $scope.menuList[index].menu_name,
-				editMenuId : $scope.menuList[index].menu_id,
-				editMenuParentId : null,
-			}
-		}
+        $scope.edit_first_menu = function(index) {
+            $scope.menuModalTitle = "修改一级菜单";
+            $scope.editMenuInfo = {
+                editMenuType: 'editFirst',
+                editMenuName: $scope.menuList[index].menu_name,
+                editMenuId: $scope.menuList[index].menu_id,
+                editMenuParentId: null,
+            }
+        }
 
-		$scope.remove_first_menu = function (index) {
-			// $scope.menuList.splice(index, 1);	
-			$http({
-		        method  : 'POST',
-		        url     : removeFirstMenuUrl,
-		        data    : "menu_id="+$scope.menuList[index].menu_id,  // pass in data as strings
-		        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-		    })
-		        .success(function(data) {
-		            if (data.status === 1) {
-		            	showTips('success',data.info);
-		            } else {
-		            	showTips('error',data.info);
-		            }
-		 			getInfo();
-		        });
-		}
+        $scope.remove_first_menu = function(index) {
+            // $scope.menuList.splice(index, 1);	
+            $http({
+                method: 'POST',
+                url: removeFirstMenuUrl,
+                data: "menu_id=" + $scope.menuList[index].menu_id, // pass in data as strings
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                } // set the headers so angular passing info as form data (not request payload)
+            })
+                .success(function(data) {
+                    if (data.status === 1) {
+                        showTips('success', data.info);
+                    } else {
+                        showTips('error', data.info);
+                    }
+                    getInfo();
+                });
+        }
 
-		$scope.edit_second_menu = function (parentIndex,index) {
-			$scope.menuModalTitle = "修改二级菜单";
-			$scope.editMenuInfo = {
-				editMenuType : 'editSecond',
-				editMenuName: $scope.menuList[parentIndex].secondMenuList[index].menu_name,
-				editMenuId :  $scope.menuList[parentIndex].secondMenuList[index].menu_id,
-				editMenuParentId : null,
-			}
-		}
+        $scope.edit_second_menu = function(parentIndex, index) {
+            $scope.menuModalTitle = "修改二级菜单";
+            $scope.editMenuInfo = {
+                editMenuType: 'editSecond',
+                editMenuName: $scope.menuList[parentIndex].secondMenuList[index].menu_name,
+                editMenuId: $scope.menuList[parentIndex].secondMenuList[index].menu_id,
+                editMenuParentId: null,
+            }
+        }
 
-		$scope.add_second_menu = function (index) {
-			if ($scope.menuList[index].secondMenuList == null||$scope.menuList[index].secondMenuList.length < 5) {
-				$scope.menuModalTitle = "添加二级菜单";
-				$scope.editMenuInfo = {
-					editMenuType : 'addSecond',
-					editMenuName: null,
-					editMenuId :  $scope.menuList[index].menu_id,
-					editMenuParentId : null,
-				}
-				$("#menuModal").modal('show');
-			} else {
-				showTips('error',"对不起，最多只能有5个二级菜单！");
-			}
-		}
+        $scope.add_second_menu = function(index) {
+            if ($scope.menuList[index].secondMenuList == null || $scope.menuList[index].secondMenuList.length < 5) {
+                $scope.menuModalTitle = "添加二级菜单";
+                $scope.editMenuInfo = {
+                    editMenuType: 'addSecond',
+                    editMenuName: null,
+                    editMenuId: $scope.menuList[index].menu_id,
+                    editMenuParentId: null,
+                }
+                $("#menuModal").modal('show');
+            } else {
+                showTips('error', "对不起，最多只能有5个二级菜单！");
+            }
+        }
 
-		$scope.remove_second_menu = function (parentIndex,index) {
-			$http({
-		        method  : 'POST',
-		        url     : removeSecondMenuUrl,
-		        data    : "menu_id="+$scope.menuList[parentIndex].secondMenuList[index].menu_id,  // pass in data as strings
-		        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-		    })
-		        .success(function(data) {
-		            if (data.status === 1) {
-		            	showTips('success',data.info);
-		            } else {
-		            	showTips('error',data.info);
-		            }
-		 			getInfo();
-		        });
-		}
+        $scope.remove_second_menu = function(parentIndex, index) {
+            $http({
+                method: 'POST',
+                url: removeSecondMenuUrl,
+                data: "menu_id=" + $scope.menuList[parentIndex].secondMenuList[index].menu_id, // pass in data as strings
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                } // set the headers so angular passing info as form data (not request payload)
+            })
+                .success(function(data) {
+                    if (data.status === 1) {
+                        showTips('success', data.info);
+                    } else {
+                        showTips('error', data.info);
+                    }
+                    getInfo();
+                });
+        }
 
-		$scope.save_menu_name = function (argument) {
-			if ($scope.editMenuInfo.editMenuName != "" && $scope.editMenuInfo.editMenuName != null) {
-				if ($scope.editMenuInfo.editMenuName.length < 6) {
-					$http({
-				        method  : 'POST',
-				        url     : saveMenuInfoUrl,
-				        data    : $.param($scope.editMenuInfo),  // pass in data as strings
-				        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-				    })
-				        .success(function(data) {
-				            if (data.status === 1) {
-				            	showTips('success',data.info);
-				            } else {
-				            	showTips('error',data.info);
-				            }
-				 			getInfo();
-				    });
-				} else {
-					showTips('error',"对不起， 菜单名称不能超过5个字！");
-				}
-			} else {
-				showTips('error',"对不起， 菜单名称不能为空！");
-			}
-		}
+        $scope.save_menu_name = function(argument) {
+            if ($scope.editMenuInfo.editMenuName != "" && $scope.editMenuInfo.editMenuName != null) {
+                if ($scope.editMenuInfo.editMenuName.length < 6) {
+                    $http({
+                        method: 'POST',
+                        url: saveMenuInfoUrl,
+                        data: $.param($scope.editMenuInfo), // pass in data as strings
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        } // set the headers so angular passing info as form data (not request payload)
+                    })
+                        .success(function(data) {
+                            if (data.status === 1) {
+                                showTips('success', data.info);
+                            } else {
+                                showTips('error', data.info);
+                            }
+                            getInfo();
+                        });
+                } else {
+                    showTips('error', "对不起， 菜单名称不能超过5个字！");
+                }
+            } else {
+                showTips('error', "对不起， 菜单名称不能为空！");
+            }
+        }
 
-		$scope.editMenuMsg = function (index, parentIndex) {
-			// 当前为二级菜单
-			if (parentIndex != null) {
-				if ($scope.menuList[parentIndex].secondMenuList[index].menu_type == "" || $scope.menuList[parentIndex].secondMenuList[index].menu_type == null) {
-					// 未设置消息
-					$scope.defaultCss = {
-						hasChild: false,
-						toChoose: true,
-						settedText: false
-					}
-				} else {
-					// 已经设置
-					$scope.defaultCss = {
-						hasChild: false,
-						toChoose: false
-					}
+        $scope.editMenuMsg = function(index, parentIndex) {
+            // 当前为二级菜单
+            if (parentIndex != null) {
+                if ($scope.menuList[parentIndex].secondMenuList[index].menu_type == "" || $scope.menuList[parentIndex].secondMenuList[index].menu_type == null) {
+                    // 未设置消息
+                    $scope.defaultCss = {
+                        hasChild: false,
+                        toChoose: true,
+                        settedText: false,
+                        toSetTextMsg: false,
+                    }
+                    $scope.editTextMsg = {
+	            		msg_type: 'text',
+	            		msg_key: $scope.menuList[parentIndex].secondMenuList[index].menu_key,
+	            	};
+                } else {
+                    // 已经设置
+                    $scope.defaultCss = {
+                        hasChild: false,
+                        toChoose: false,
+                        toSetTextMsg: false,
+                    }
 
-					if ($scope.menuList[parentIndex].secondMenuList[index].menu_type == "text") {
-						
-						$http({
-					        method  : 'POST',
-					        url     : getSecondMenuInfoUrl,
-					        data    : 's_id=' + $scope.menuList[parentIndex].secondMenuList[index].menu_id,
-					        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-					    })
-				        .success(function(data) {
-				        	$scope.defaultCss.settedText = true;
-				        	if (data.status == 1) {
-				        		$scope.settedTextValue = data.data.msg_items.msg_text;
-				        	} else {
-				        		showTips('error',"对不起，数据获取失败！");
-				        	}
-				        })
-				        .error(function(data) {
-				        	showTips('error',"对不起，连接服务器失败！");
-				        });
-						
-					};
-				}
-				
-			} else {
-				// 当前("一级菜单");
-				if ($scope.menuList[index].secondMenuList == null) {
-					// 没有子菜单，需设置消息
-					$scope.defaultCss = {
-						hasChild: false,
-						toChoose: true,
-						settedText: false
-					}
-				} else {
-					// 有子节点，不能设置消息
-					$scope.defaultCss = {
-						hasChild: true,
-						toChoose: false,
-						settedText: false
-					}
-				}
-			}
-			
-		}
-	}
+                    if ($scope.menuList[parentIndex].secondMenuList[index].menu_type == "text") {
+
+                        $http({
+                            method: 'POST',
+                            url: getSecondMenuInfoUrl,
+                            data: 's_id=' + $scope.menuList[parentIndex].secondMenuList[index].menu_id,
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                            } // set the headers so angular passing info as form data (not request payload)
+                        })
+                            .success(function(data) {
+                                $scope.defaultCss.settedText = true;
+                                if (data.status == 1) {
+                                    $scope.settedTextValue = data.data.msg_items.msg_text;
+                                    $scope.editTextMsg = data.data.msg_items;
+                                } else {
+                                    showTips('error', "对不起，数据获取失败！");
+                                }
+                            })
+                            .error(function(data) {
+                                showTips('error', "对不起，连接服务器失败！");
+                            });
+
+                    };
+                }
+
+            } else {
+                // 当前("一级菜单");
+                if ($scope.menuList[index].secondMenuList == null) {
+                    // 没有子菜单，需设置消息
+                    $scope.defaultCss = {
+                        hasChild: false,
+                        toChoose: true,
+                        settedText: false,
+                        toSetTextMsg: false,
+                    }
+                    $scope.editTextMsg = {
+	            		msg_type: 'text',
+	            		msg_key: $scope.menuList[index].menu_key,
+	            	};
+                } else {
+                    // 有子节点，不能设置消息
+                    $scope.defaultCss = {
+                        hasChild: true,
+                        toChoose: false,
+                        settedText: false,
+                        toSetTextMsg: false,
+                    }
+                }
+            }
+        }
+
+        $scope.saveEditTextMsgClick = function() {
+            // editTextMsg
+        }
+
+        $scope.toShowMsgEdit = function() {
+            $scope.defaultCss = {
+                hasChild: false,
+                toChoose: false,
+                settedText: false,
+                toSetTextMsg: true,
+            }
+        }
+    }
 ]);
