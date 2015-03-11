bdf40f7770e650690a3c3aa1bddc509d7c9f1598
493405455 <zhuyunxiang@users.noreply.github.com>
2015-03-11 15:18:51 +0000

    1.用UI.router实现路由

--- 1/Public/home/js/home/home.js
+++ 1/Public/home/js/home/home.js
@@ -1,15 +1,18 @@
-var app = angular.module('HomeApp', ['ngRoute', 'HomeCtrls']);
+var app = angular.module('HomeApp', ['ui.router', 'HomeCtrls']);
 
-app.config(function($routeProvider) {
-    $routeProvider.when('/home',
-    {
-        templateUrl: publicUrl+'tpl/home/home.html',
-        controller: "homeCtrl"
-    }).
-    when('/register',
-    {
-        templateUrl: publicUrl+'tpl/home/register.html',
-        controller: "registerCtrl"
-    }).
-    otherwise({ redirectTo: '/home' });
+app.config(function($stateProvider, $urlRouterProvider) {
+    // 设置默认显示的页面
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: publicUrl + 'tpl/home/home.html',
+            controller: "homeCtrl"
+        })
+        .state('register', {
+            url: "/register",
+            templateUrl: publicUrl + 'tpl/home/register.html',
+            controller: "registerCtrl"
+        });
 });
