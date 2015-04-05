182b7a93dc6e7dc23d523ed20776346a26b1dc26
zhao_yuxue <zhaoyuxue>
2015-04-05 12:16:25 +0000

    1. 修改主页样式 2. 添加右边内容

--- 1/Public/home/js/home/homeCtrls.js
+++ 1/Public/home/js/home/homeCtrls.js
@@ -1,50 +1,69 @@
 var HomeCtrls = angular.module('HomeCtrls', []);
 
 HomeCtrls.controller('homeCtrl', ['$scope',
-	function($scope){
-		$scope.userInfo = {email: "tests@qq.com"};
+    function($scope) {
+        $scope.userInfo = {
+            email: "tests@qq.com"
+        };
 
-		$scope.tplURLs = {
-			pictureShow: publicUrl+'tpl/home/picture_show.html',
-			leftNavOuterContainer: publicUrl+'tpl/home/nav_containers.html',
-			superAdContainer: publicUrl+'tpl/home/super_ad.html',
-		};
+        $scope.tplURLs = {
+            pictureShow: publicUrl + 'tpl/home/picture_show.html',
+            leftNavOuterContainer: publicUrl + 'tpl/home/nav_containers.html',
+            superAdContainer: publicUrl + 'tpl/home/super_ad.html',
+            rightNavOuterContainer: publicUrl + 'tpl/home/right_container.html',
+        };
 
-		$scope.imageURLs = {
-			'publicUrl': publicUrl,
-			'logo': commenUrl+'img/logo-mini.png',
-		};
-	}
+        $scope.imageURLs = {
+            'publicUrl': publicUrl,
+            'logo': commenUrl + 'img/logo-mini.png',
+        };
+
+        $scope.fasionClothes = {
+            0: [
+            {"class": "type-name","content": "潮流女装"},
+            {"class": "commen","content": "春新品"},
+            {"class": "hot","content": "韩版"},
+            {"class": "commen","content": "毛衣"},
+            {"class": "commen","content": "薄开衫"},
+            {"class": "commen","content": "本身裙"},
+            ],
+
+            1: [
+            {"class": "type-name","content": "潮流女装"},
+            {"class": "commen","content": "春装"},
+            ],
+        };
+    }
 ]);
 
 HomeCtrls.controller('registerCtrl', ['$scope', '$http',
-	function($scope, $http) {
-		$scope.resetUserInfo = function () {
-			$scope.userInfo = {
-				email: "",
-				password: "",
-				repassword: "",
-				autoLogin: false
-			};
-		}
+    function($scope, $http) {
+        $scope.resetUserInfo = function() {
+            $scope.userInfo = {
+                email: "",
+                password: "",
+                repassword: "",
+                autoLogin: false
+            };
+        }
 
-		$scope.submitInfo = function () {
-			$http({
-        		method: 'POST',
-			    url: appIndexUrl+'Index/doRegister',
-			    headers: {
-				 	'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-				}, 
+        $scope.submitInfo = function() {
+            $http({
+                method: 'POST',
+                url: appIndexUrl + 'Index/doRegister',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                },
 
-	   			data: $scope.userInfo
+                data: $scope.userInfo
 
-			}).success(function(data, status, headers, config) {
-			    console.log("success...");
-			    console.log(data);
-			    $scope.users=data;
-			}).error(function(data, status, headers, config) {
-			    console.log("error...");
-			});
-		}
-	}
+            }).success(function(data, status, headers, config) {
+                console.log("success...");
+                console.log(data);
+                $scope.users = data;
+            }).error(function(data, status, headers, config) {
+                console.log("error...");
+            });
+        }
+    }
 ]);
