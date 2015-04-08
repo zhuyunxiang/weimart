160e4c38fd9cac253f52da3540ba40ed53492c50
493405455 <zhuyunxiang@users.noreply.github.com>
2015-04-08 13:41:25 +0000

    1. 添加首页的其他分页面

--- 1/Public/home/js/home/home.js
+++ 1/Public/home/js/home/home.js
@@ -13,6 +13,22 @@ app.config(function($stateProvider, $urlRouterProvider) {
             url: "/hot_baby",
             templateUrl: publicUrl + 'tpl/home/hot_baby.html',
             controller: "hotBabyCtrl"
+        }).state('popular_brand', {
+            url: "/popular_brand",
+            templateUrl: publicUrl + 'tpl/home/popular_brand.html',
+            controller: "popularBrandCtrl"
+        }).state('popular_shop', {
+            url: "/popular_shop",
+            templateUrl: publicUrl + 'tpl/home/popular_shop.html',
+            controller: "popularShopCtrl"
+        }).state('delegate', {
+            url: "/delegate",
+            templateUrl: publicUrl + 'tpl/home/delegate.html',
+            controller: "delegateCtrl"
+        }).state('more_cheap', {
+            url: "/more_cheap",
+            templateUrl: publicUrl + 'tpl/home/more_cheap.html',
+            controller: "moreCheapCtrl"
         })
         .state('register', {
             url: "/register",
