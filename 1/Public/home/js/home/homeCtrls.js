4b39dcefbc054379e7150e1ab4ae5c074e962893
493405455@qq.com <493405455@qq.com@b65dde92-2312-485a-a083-f5afe3003cf0>
2015-04-09 16:39:12 +0000

    1. 完成热门品牌的实现

--- 1/Public/home/js/home/homeCtrls.js
+++ 1/Public/home/js/home/homeCtrls.js
@@ -66,8 +66,45 @@ HomeCtrls.controller('popularBrandCtrl', ['$scope',
         };
 
         $scope.tplURLs = {
-            
+            popularBrandNav : publicUrl + 'tpl/home/popular_brand_nav.html',
+            popularBrandMain : publicUrl + 'tpl/home/popular_brand_main.html',
         };
+
+        $scope.brandList = [
+        {"url": "1.jpg"},
+        {"url": "2.jpg"},
+        {"url": "3.jpg"},
+        {"url": "4.jpg"},
+        {"url": "5.jpg"},
+        {"url": "6.jpg"},
+        {"url": "7.jpg"},
+        {"url": "8.jpg"},
+        {"url": "9.jpg"},
+        {"url": "10.jpg"},
+        {"url": "11.jpg"},
+        {"url": "12.jpg"},
+        {"url": "13.jpg"},
+        {"url": "14.jpg"},
+        {"url": "15.jpg"},
+        {"url": "16.jpg"},
+        {"url": "17.jpg"},
+        {"url": "18.jpg"},
+        {"url": "19.jpg"},
+        {"url": "20.jpg"},
+        {"url": "21.jpg"},
+        {"url": "22.jpg"},
+        {"url": "23.jpg"},
+        {"url": "24.jpg"},
+        ];
+
+        $scope.item_list = [
+            {"url": "25.jpg"},
+            {"url": "26.jpg"},
+            {"url": "27.jpg"},
+            {"url": "28.jpg"},
+            {"url": "29.jpg"},
+            {"url": "30.jpg"},
+        ];
     }
 ]);
 // 热卖品牌的Controller End
