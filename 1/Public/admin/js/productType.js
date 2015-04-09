d540b7088641aa4f69486296ba28380644cf6fe2
493405455 <zhuyunxiang@users.noreply.github.com>
2015-04-09 14:15:19 +0000

    1. 完成angular tree在商品类别管理上的移植

--- 1/Public/admin/js/productType.js
+++
@@ -1,7 +0,0 @@
-var myAppModule = angular.module('MyApp', ['ui.tree']);
-var myController = myAppModule.controller('myController', ['$scope', function($scope){
-	$scope.list = [
-		{title: '周树青',items:[{title:'朱云祥',items:[{title:'朱云祥'}]}]},
-		{title: '支广生',items:[{title:'许一超'}]},
-	];
-}]);
