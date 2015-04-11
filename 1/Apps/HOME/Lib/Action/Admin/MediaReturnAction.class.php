02d559c276946c01544863d07f73b0a797d04866
493405455 <zhuyunxiang@users.noreply.github.com>
2015-04-11 19:14:18 +0000

    1. 解决图片上传BUG

--- 1/Apps/HOME/Lib/Action/Admin/MediaReturnAction.class.php
+++ 1/Apps/HOME/Lib/Action/Admin/MediaReturnAction.class.php
@@ -9,12 +9,35 @@ class MediaReturnAction extends Action
 		$this->display();
 	}
 
+	// public function save_img()
+	// {
+	// 	$filename = time().$_FILES['file']['name'];
+	// 	$destination = "./upload/Uploads/media_img/" . $filename;
+	// 	move_uploaded_file( $_FILES['file']['tmp_name'] , $destination );
+	// 	echo $filename;
+	// }
+
 	public function save_img()
 	{
-		$filename = time().$_FILES['file']['name'];
-		$destination = "./Public/Uploads/media_img/" . $filename;
-		move_uploaded_file( $_FILES['file']['tmp_name'] , $destination );
-		echo $filename;
+		if (!empty($_FILES)) {
+            import("@.ORG.UploadFile");
+            $config=array(
+                'allowExts'=>array('jpg','gif','png'),
+                'savePath'=>'./Public/Uploads/media_img/',
+                // 'saveRule'=>'time',
+            );
+            $upload = new UploadFile($config);
+            $upload->thumb=true;
+            $upload->thumbMaxHeight=100;
+            $upload->thumbMaxWidth=100;
+            if (!$upload->upload()) {
+                $this->error($upload->getErrorMsg());
+            } else {
+            	$id = $_POST['now_id'];
+                $info = $upload->getUploadFileInfo();
+				echo $info[0]['savename'];
+            }
+		}
 	}
 
 }
