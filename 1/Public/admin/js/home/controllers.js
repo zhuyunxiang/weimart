23c82971703bebabf5b476485c90ae9fcab7fcf8
493405455 <zhuyunxiang@users.noreply.github.com>
2015-04-19 14:48:29 +0000

    1. 完成人员管理

--- 1/Public/admin/js/home/controllers.js
+++ 1/Public/admin/js/home/controllers.js
@@ -1,5 +1,24 @@
 var controllers = angular.module('controllers', []);
 
-controllers.controller('adminCtrl', ['$scope', function($scope){
-	$scope.navContent = "欢迎访问";
-}]);
+controllers.controller('adminCtrl', ['$scope',
+    function($scope) {
+        $scope.navContent = "欢迎访问";
+    }
+]);
+
+controllers.controller('userAdminCtrl', ['$scope', 'User',
+    function($scope, User) {
+
+        User.getUserList();
+
+        $scope.$on('User.getUserListSuccess', function(event) {
+            $scope.user_list = User.user_list;
+        });
+
+        $scope.removeUserInfo = function(userId) {
+            if (confirm("确定要删除?不可回复!")) {
+                User.removeUserInfo(userId);
+            };
+        }
+    }
+])
