5f58134b44771e20e284676dccabc0fd981d625d
493405455 <zhuyunxiang@users.noreply.github.com>
2015-06-09 14:47:23 +0000

    1. 添加分享到社交平台

--- 1/Public/home/js/home/homeCtrls.js
+++ 1/Public/home/js/home/homeCtrls.js
@@ -787,7 +787,47 @@ HomeCtrls.controller('productDetailCtrl', ['$scope', '$state', '$stateParams', '
         $scope.imageURLs = {
             'publicUrl': publicUrl,
             'logo': commenUrl + 'img/logo-mini.png',
+            'qq': publicUrl+ 'img/share/qq.png',
+            'weibo': publicUrl+ 'img/share/weibo.png',
+            'qqweibo': publicUrl+ 'img/share/qqweibo.png'
         };
+
+        var ShareTip = function() {
+
+        }
+        //分享到腾讯微博
+        ShareTip.prototype.sharetoqq = function(content, url, picurl) {
+            var shareqqstring = 'http://v.t.qq.com/share/share.php?title=' + content + '&url=' + url + '&pic=' + picurl;
+            window.open(shareqqstring, 'newwindow', 'height=100,width=100,top=100,left=100');
+        }
+        //分享到新浪微博
+        ShareTip.prototype.sharetosina = function(title, url, picurl) {
+            var sharesinastring = 'http://v.t.sina.com.cn/share/share.php?title=' + title + '&url=' + url + '&content=utf-8&sourceUrl=' + url + '&pic=' + picurl;
+            window.open(sharesinastring, 'newwindow', 'height=400,width=400,top=100,left=100');
+        }
+        //分享到QQ空间
+        ShareTip.prototype.sharetoqqzone = function(title, url, picurl) {
+            var shareqqzonestring = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?summary=' + title + '&url=' + url + '&pics=' + picurl;
+            window.open(shareqqzonestring, 'newwindow', 'height=400,width=400,top=100,left=100');
+        }
+
+        var share1 = new ShareTip();  
+        $scope.shareClick = function (type) {
+            var str = "大家快来看看我在卖盟上看中的宝贝吧！";
+            var url = "http://weimart.sinaapp.com/index.php/Index/home#/product_detail/"+$scope.productInfo.product_id;
+            var imgUrl = $scope.productInfo.product_img;
+            if (type == 'qq') {
+                share1.sharetoqqzone(str,url,imgUrl);
+            };
+            if (type == 'weibo') {
+                share1.sharetosina(str,url,imgUrl);
+            };
+
+            if (type == 'qqweibo') {
+                share1.sharetoqq(str,url,imgUrl);
+            };
+        }
+        
 
         Product.getCurrentProduct($stateParams.id);
         // 监听收藏/取消收藏商品成功
